1f29111281375c6b0b5874c8a26c686631911b54
Tayyba Aftab <144408026+tayyba678@users.noreply.github.com>
2023-10-13 12:04:28 +0500

    Add files via upload

--- 4.cpp
+++ 4.cpp
@@ -1,12 +1,59 @@
 #include<iostream>
 using namespace std;
-int main(){
-cout<<"Enter length of rectangle:"<<endl;
-int length;
-cin>> length;
-cout<< "Enter width of rectangle:"<<endl;
-int area,width;
-cin>> width;
-area=length*width;
-cout<<"The area of rectangle is:"<<area<<endl;
+string serviceCode(char a);
+float minutes(char d,int mi);
+
+main()
+{
+char s,t;
+int m;
+string k;
+float f;
+cout<<"Enterb the service code (R/r for regular, P/p for premium): ";
+cin>>s;
+cout<<"Enter time of thr call (D/d for day, N/n for night): ";
+cin>>t;
+cout<<"Enter thr number of minutes used: ";
+cin >>m;
+k=serviceCode(s);
+cout<<"Service Type: "<<k<<endl;;
+cout<<"Total minutes Used: "<<m<<" minutes"<<endl;
+f=minutes(t,m);
+cout<<"Amount Due: $"<<f;
+
 }
+string serviceCode(char a)
+{
+string h;
+if(a=='P'||a=='p')
+{
+h="Premium";
+}
+if(a=='R'||a=='r')
+{
+h="Regular";
+}
+return h;
+}
+float minutes(char d,int mi)
+{
+float u;
+if(d=='D'||d=='d')
+{
+if(mi>75)
+{
+u=mi*0.10;
+}
+}
+if(d=='N'||d=='n')
+{
+if(mi>100)
+{
+u=mi*0.05;
+}
+}
+return u;
+}
+
+
+
